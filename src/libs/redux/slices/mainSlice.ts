0fb819daafac2f1e63055156db76cbb6435f3753
icestormikk <jigalow.pavel2015@yandex.ru>
2024-05-05 16:09:05 +0300

    Добавил поле и редюсеры для хранения и изменения поля, отвечающего за ответ, полученный от алгоритма

--- src/libs/redux/slices/mainSlice.ts
+++ src/libs/redux/slices/mainSlice.ts
@@ -1,15 +1,18 @@
+import { ProgramResponse } from "@/types/ProgramResponse"
 import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 
 interface MainState {
   executableFilePath?: string
   logFilePath?: string
-  resultsFilePath?: string
+  resultsFilePath?: string,
+  response?: ProgramResponse
 }
 
 const initialState: MainState = {
   executableFilePath: "C:\\Users\\jigal\\IdeaProjects\\chinese_postman_problem\\build\\libs\\chinese_postman_problem-1.1-SNAPSHOT-standalone.jar",
   logFilePath: "C:\\Users\\jigal\\chinese-postman-problem-program.log",
-  resultsFilePath: undefined
+  resultsFilePath: undefined,
+  response: undefined 
 }
 
 const mainSlice = createSlice({
@@ -27,9 +30,18 @@ const mainSlice = createSlice({
     setResultsFilePath: (state, action: PayloadAction<string>) => {
       const path = action.payload
       state.resultsFilePath = path
+    },
+    setResponse: (state, action: PayloadAction<ProgramResponse|undefined>) => {
+      const response = action.payload
+      state.response = response
     }
   }
 })
 
-export const { setExecutableFilePath, setLogFilePath, setResultsFilePath } = mainSlice.actions
+export const { 
+  setExecutableFilePath, 
+  setLogFilePath, 
+  setResultsFilePath,
+  setResponse
+} = mainSlice.actions
 export default mainSlice.reducer
