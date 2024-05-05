c9e14dde37ef26623f7596baf91e671c67277e9c
icestormikk <jigalow.pavel2015@yandex.ru>
2024-05-05 16:10:04 +0300

    Добавил логику оформления ответа алгоритма и внесения его в хранилище

--- src/screens/algorithm/LaunchGenetic.tsx
+++ src/screens/algorithm/LaunchGenetic.tsx
@@ -1,4 +1,4 @@
-import { useAppSelector } from '@/libs/redux/hooks';
+import { useAppDispatch, useAppSelector } from '@/libs/redux/hooks';
 import { getTranslatedMutationType, MutationTypes } from '@/types/enums/MutationTypes';
 import { getTranslatedParentsChooserType, ParentsChooserTypes } from '@/types/enums/ParentsChooserTypes';
 import { getTranslatedParentsSelectionType, ParentsSelectionType } from '@/types/enums/ParentsSelectionTypes';
@@ -10,21 +10,21 @@ import { getNewPopulationSelectionType, NewPopulationSelectionTypes } from '@/ty
 import { GeneticAlgorithmFormProps, GeneticAlgorithmProps } from '@/types/alogrithms/GeneticAlgorithmProps';
 import { VscDebugStart, VscLoading } from 'react-icons/vsc';
 import { Graph } from '@/types/graph/Graph';
-import SuccessMessage from '@/components/messages/SuccessMessage';
-import ErrorMessage from '@/components/messages/ErrorMessage';
+import { setResponse } from '@/libs/redux/slices/mainSlice';
+import { ProgramResponse } from '@/types/ProgramResponse';
 
 function LaunchGenetic() {
+  const dispatch = useAppDispatch()
   const { executableFilePath, logFilePath, resultsFilePath } = useAppSelector((state) => state.main)
   const { nodes, edges } = useAppSelector((state) => state.graph)
   const [isWorking, setIsWorking] = React.useState(false)
   const [errors, setErrors] = React.useState<string[]>([])
-  const [status, setStatus] = React.useState<"SUCCESS"|"FAILED">()
 
   const onGeneticAlgorithmLaunch = React.useCallback(
     async (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault()
       setIsWorking(true)
-      setStatus(undefined)
+      dispatch(setResponse(undefined))
       if (!logFilePath || !executableFilePath || !resultsFilePath) return
 
       const target = e.target as typeof e.target & GeneticAlgorithmFormProps
@@ -42,24 +42,45 @@ function LaunchGenetic() {
         newPopulationRate: Number(target.newPopulationRate.value)
       }
       
+      const dateStart = new Date()
       window.electron
         .launchGeneticAlgorithm(
           logFilePath, executableFilePath, resultsFilePath, data, new Graph(nodes, edges)
         )
-        .then(() => {
+        .then(async () => {
+          const dateEnd = new Date()
+          console.log(dateEnd.getTime() - dateStart.getTime())
           setIsWorking(false)
-          setStatus("SUCCESS")
+          const content = await window.electron.readFile(resultsFilePath)
+          const result = content as unknown as Array<string>
+          dispatch(
+            setResponse({
+              code: "SUCCESS",
+              data: {
+                message: "Алгоритм завершил свою работу успешно",
+                result
+              }
+            })
+          )
         })
         .catch((err) => {
           setIsWorking(false)
-          setStatus("FAILED")
+          dispatch(
+            setResponse({
+              code: "FAILED",
+              data: {
+                message: err.message,
+                possibleSolution: 'Проверьте правильность заполнения полей в конфигурации'
+              }
+            })
+          )
         })
     },
     []
   )
 
   return (
-    <form onSubmit={onGeneticAlgorithmLaunch} className='flex flex-col gap-2 p-2 rounded-md shadow-md border border-solid border-gray-200 w-full'>
+    <form onSubmit={onGeneticAlgorithmLaunch} className='flex flex-col gap-2 p-2 rounded-md shadow-md border border-solid border-gray-200'>
       <CustomFormField id='iterationsCount' title={'Количество итераций алгоритма'}>
         <input type="number" name="iterationsCount" id="iterationsCount" defaultValue={1} min={1} required/>
       </CustomFormField>
@@ -151,12 +172,6 @@ function LaunchGenetic() {
           {isWorking ? 'Вычисляем...' : 'Запустить'}
           {isWorking ? <VscLoading size={20} className='animate-spin'/> : <VscDebugStart size={20}/>}
         </button>
-        {
-          status === "SUCCESS" && ( <SuccessMessage message={'Алгоритм завершил свою работу успешно'}/> )
-        }
-        {
-          status === "FAILED" && ( <ErrorMessage message={'Во время работы алгоритма произошла ошибка'}/> )
-        }
       </div>
     </form>
   );
