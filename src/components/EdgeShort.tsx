c1ac0ea1436e74a1f011b6ca9aa6a0f1d1db7749
icestormikk <jigalow.pavel2015@yandex.ru>
2024-05-06 19:54:35 +0300

    Добавил на компоненты с информацией о ребрах и вершинах кнопку, которая позволяет удалить конкретную сущность из графа

--- src/components/EdgeShort.tsx
+++ src/components/EdgeShort.tsx
@@ -1,18 +1,33 @@
+import { useAppDispatch } from '@/libs/redux/hooks';
+import { removeEdgeById } from '@/libs/redux/slices/graphSlice';
 import { Edge } from '@/types/graph/Edge';
-import { EdgeType } from '@/types/graph/EdgeType';
 import React from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
 
 interface EdgeShortProps {
   edge: Edge<number>
 }
 
 const EdgeShort = ({ edge }: EdgeShortProps) => {
+  const dispatch = useAppDispatch()
+  const onEdgeRemove = React.useCallback(
+    () => {
+      dispatch(removeEdgeById(edge.id))
+    },
+    []
+  )
+
   return (
-    <div className='bg-gray-200 border-[1px] border-solid border-gray-300 rounded-md p-2 flex flex-col'>
-      <span><b>{edge.source.label}</b> - <b>{edge.destination.label}</b></span>
-      <span className='text-sm'>Вес ребра: <b>{edge.weight}</b></span>
-      <span className='text-sm'>Тип ребра: <b>{edge.type}</b></span>
-      <span className='text-xs text-gray-400'>{edge.id}</span>
+    <div className='bg-gray-200 border-[1px] border-solid border-gray-300 rounded-md p-2 flex flex-row justify-between items-center'>
+      <div className='flex flex-col gap-2'>
+        <span><b>{edge.source.label}</b> - <b>{edge.destination.label}</b></span>
+        <span className='text-sm'>Вес ребра: <b>{edge.weight}</b></span>
+        <span className='text-sm'>Тип ребра: <b>{edge.type}</b></span>
+        <span className='text-xs text-gray-400'>{edge.id}</span>
+      </div>
+      <button className='bg-transparent' onClick={() => onEdgeRemove()}>
+        <AiOutlineClose/>
+      </button>
     </div>
   );
 };
--- src/components/NodeShort.tsx
+++ src/components/NodeShort.tsx
@@ -1,15 +1,31 @@
+import { useAppDispatch } from '@/libs/redux/hooks';
+import { removeNodeById } from '@/libs/redux/slices/graphSlice';
 import { Node } from '@/types/graph/Node';
 import React from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
 
 interface NodeShortProps {
   node: Node
 }
 
 const NodeShort = ({ node }: NodeShortProps) => {
+  const dispatch = useAppDispatch()
+  const onNodeRemove = React.useCallback(
+    () => {
+      dispatch(removeNodeById(node.id))
+    },
+    []
+  )
+
   return (
-    <div className='bg-gray-200 border-[1px] border-solid border-gray-300 rounded-md p-2 flex flex-col'>
-      <b>{node.label}</b>
-      <span className='text-xs text-gray-400'>{node.id}</span>
+    <div className='bg-gray-200 border-[1px] border-solid border-gray-300 rounded-md p-2 flex flex-row justify-between items-center'>
+      <div className='flex flex-col'>
+        <b>{node.label}</b>
+        <span className='text-xs text-gray-400'>{node.id}</span>
+      </div>
+      <button className='bg-transparent' onClick={() => onNodeRemove()}>
+        <AiOutlineClose/>
+      </button>
     </div>
   );
 };
