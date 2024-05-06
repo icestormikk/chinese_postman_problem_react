1aba107a3427d51f8b06626d9aa5cd385be3a0ee
icestormikk <jigalow.pavel2015@yandex.ru>
2024-05-06 22:16:44 +0300

    Добавил на кнопку стилей, которые применяются при выполнении определённых условий, и обновил свойство  onClick

--- src/components/sidebar/SidebarButton.tsx
+++ src/components/sidebar/SidebarButton.tsx
@@ -1,12 +1,13 @@
 import { SidebarButtonProps } from '@/types/SidebarButtonProps';
 import React from 'react';
 
-function SidebarButton({ title, children, onClick }: React.PropsWithChildren<Omit<SidebarButtonProps, 'icon'>>) {
+function SidebarButton({ title, children, onClick, isDisabled = false }: React.PropsWithChildren<Omit<SidebarButtonProps, 'icon'>>) {
   return (
     <div 
       className='h-14 aspect-square hover:bg-gray-400/50 bg-transparent flex justify-center items-center font-bold'
-      onClick={onClick}
-      title={title}
+      style={{opacity: isDisabled ? 0.5 : 1.0}}
+      onClick={isDisabled ? () => {} : onClick}
+      title={title + " " + (isDisabled ? '(заблокировано)' : '')}
     >
       {children}
     </div>
