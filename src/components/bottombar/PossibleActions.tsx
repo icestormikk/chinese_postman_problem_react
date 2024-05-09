8369e4cfcebb82bb5a572839cdc2a8378c5dae2c
icestormikk <jigalow.pavel2015@yandex.ru>
2024-05-09 20:47:18 +0300

    Временно убраны возможности нижнего меню

--- src/components/bottombar/PossibleActions.tsx
+++ src/components/bottombar/PossibleActions.tsx
@@ -1,36 +1,16 @@
 import React from 'react';
 import BottomBarAction, { BottomBarActionProps } from './BottomBarAction';
 import { useAppSelector } from '@/libs/redux/hooks';
-import { FaMinus, FaPlus } from 'react-icons/fa6';
 
 function PossibleActions() {
-  const { edges, nodes, selectedNodes } = useAppSelector((state) => state.graph)
+  const { edges, nodes } = useAppSelector((state) => state.graph)
   
   const actions = React.useMemo<BottomBarActionProps[]>(
     () => {
       const res: BottomBarActionProps[] = []
-
-      if (selectedNodes.length === 2) {
-        const sourceNode = nodes.find((n) => n.id === selectedNodes[0])
-        const destinationNode = nodes.find((n) => n.id === selectedNodes[1])
-
-        if (!sourceNode || !destinationNode) return []
-
-        const edge = edges.find((e) => e.source.id === selectedNodes[0] && e.destination.id === selectedNodes[1])
-        if (!edge) {
-          res.push(
-            { title: `Добавить ребро (${sourceNode.label} -> ${destinationNode.label})`, icon: <FaPlus/>, onClick: () => {} }
-          )
-        } else {
-          res.push(
-            { title: `Удалить ребро (${sourceNode.label} -> ${destinationNode.label})`, icon: <FaMinus/>, onClick: () => {} }
-          )
-        }
-      }
-
       return res
     },
-    [selectedNodes, edges, nodes]
+    [edges, nodes]
   )
 
   return (
