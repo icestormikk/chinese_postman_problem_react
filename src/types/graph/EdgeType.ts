cbe6e76f06ce7f0885a912cf9a9b73df81318bd2
icestormikk <jigalow.pavel2015@yandex.ru>
2024-05-09 20:56:13 +0300

    Добавил функцию для получения названий типов рёбер графа на русском языке

--- src/types/graph/EdgeType.ts
+++ src/types/graph/EdgeType.ts
@@ -2,3 +2,14 @@ export enum EdgeType {
   DIRECTED = 'DIRECTED', 
   NOT_ORIENTED = 'NOT_ORIENTED'
 }
+
+export function getTranslatedEdgeType(type: EdgeType): string {
+  switch (type) {
+    case EdgeType.DIRECTED: {
+      return "Ориентированное"
+    }
+    case EdgeType.NOT_ORIENTED: {
+      return "Не ориентированное"
+    }
+  }
+}
