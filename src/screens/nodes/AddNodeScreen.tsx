ce92a1f67734006be9ac441a5ed98fbd41c211d1
icestormikk <jigalow.pavel2015@yandex.ru>
2024-05-09 20:59:35 +0300

    Добавил экран для добавления вершин в граф и отображения их списка

---
+++ src/screens/nodes/AddNodeScreen.tsx
@@ -0,0 +1,73 @@
+import CustomFormField from '@/components/CustomFormField';
+import WarningMessage from '@/components/messages/WarningMessage';
+import { useAppDispatch, useAppSelector } from '@/libs/redux/hooks';
+import { setNodes } from '@/libs/redux/slices/graphSlice';
+import { Node } from '@/types/graph/Node';
+import React from 'react';
+
+interface AddNodeFormProps {
+  nodeLabel: { value: string }
+}
+
+function AddNodeScreen() {
+  const dispatch = useAppDispatch()
+  const { nodes } = useAppSelector((state) => state.graph)
+  const [warnings, setWarnings] = React.useState<string[]>([])
+  const [errors, setErrors] = React.useState<string[]>([])
+  const [userClickCount, setUserClickCount] = React.useState(0)
+
+  const onNodeCreate = React.useCallback(
+    (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault()
+      setUserClickCount((prevState) => prevState + 1)
+      setErrors([])
+      setWarnings([])
+
+      const target = event.target as typeof event.target & AddNodeFormProps
+      const data = {
+        label: target.nodeLabel.value
+      }
+
+      if (data.label.length === 0) {
+        setErrors((prevState) => [...prevState, "Название вершины не может быть пустым"])
+        return
+      }
+      
+      const isSameNameNodeContains = nodes.find((n) => n.label === data.label) != undefined
+      if (isSameNameNodeContains && userClickCount === 0) {
+        setWarnings((prevState) => [
+          ...prevState, 
+          `В графе уже содержится вершина с таким названием. Добавление вершины с таким же названием может вызвать путаницу. 
+          Для подтверждения действия нажмите ещё раз на кнопку "Добавить".`
+        ])
+        return
+      }
+
+      const node = new Node(data.label)
+      dispatch(setNodes([...nodes, node]))
+      setUserClickCount(0)
+    },
+    [nodes, errors, warnings]
+  )
+
+  return (
+    <form onSubmit={(event) => onNodeCreate(event)} >
+      <CustomFormField id={'nodeLabel'} title={'Название вершины'}>
+        <input type="text" name="nodeLabel" id="nodeLabel" required/>
+      </CustomFormField>
+      {
+        warnings.map((warning, index) => (
+          <WarningMessage key={index} message={warning} />
+        ))
+      }
+      {
+        errors.map((error, index) => (
+          <WarningMessage key={index} message={error} />
+        ))
+      }
+      <button className='bg-green-500 text-white'>Добавить</button>
+    </form>
+  );
+}
+
+export default AddNodeScreen;
--- src/screens/nodes/NodesScreen.tsx
+++ src/screens/nodes/NodesScreen.tsx
@@ -1,11 +1,18 @@
+import Modal from '@/components/Modal';
 import NodeShort from '@/components/NodeShort';
 import { useAppSelector } from '@/libs/redux/hooks';
+import React from 'react';
+import AddNodeScreen from './AddNodeScreen';
 
 function NodesScreen() {
   const { nodes } = useAppSelector((state) => state.graph)
+  const [isAddNodeModalOpen, setIsAddNodeModalOpen] = React.useState(false)
 
   return (
     <div>
+      <button className='w-full bg-green-500 text-white' onClick={() => setIsAddNodeModalOpen(true)}>
+        Добавить вершину
+      </button>
       {
         nodes.length > 0 ? (
           <div className='flex flex-col gap-2'>
@@ -18,6 +25,18 @@ function NodesScreen() {
           </div>
         ) : (
           <b className='text-gray-500'>Похоже, в графе нет вершин</b>
+        )
+      }
+      {
+        isAddNodeModalOpen && (
+          <Modal 
+            isOpen={isAddNodeModalOpen} 
+            title={'Добавление вершины в граф'}
+            hasCloseBtn={true}
+            onClose={() => setIsAddNodeModalOpen(false)}
+          >
+            <AddNodeScreen/>
+          </Modal>
         )
       }
     </div>
