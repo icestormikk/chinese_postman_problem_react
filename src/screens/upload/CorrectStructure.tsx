037ebf9b1772eeeccc48916751a28f6cd268f61e
icestormikk <jigalow.pavel2015@yandex.ru>
2024-05-11 20:02:52 +0300

    Обновил инициализацию списка ребёр для графа-заглушки для корректной структуры

--- src/screens/upload/CorrectStructure.tsx
+++ src/screens/upload/CorrectStructure.tsx
@@ -3,17 +3,18 @@ import { EdgeType } from '@/types/graph/EdgeType';
 import { Graph } from '@/types/graph/Graph';
 import { Node } from '@/types/graph/Node';
 import React from 'react';
+import { v4 } from 'uuid';
 
 const CorrectStructure = () => {
   const correctStructure = React.useMemo(
     () => {
       const nodes = [...Array(3).keys()].map((num) => new Node(`Node-${num}`))
       const edges = [
-        new Edge(nodes[0], nodes[1], 100, EdgeType.NOT_ORIENTED),
-        new Edge(nodes[0], nodes[1], 100, EdgeType.NOT_ORIENTED),
-        new Edge(nodes[0], nodes[1], 100, EdgeType.NOT_ORIENTED),
-        new Edge(nodes[1], nodes[2], 100, EdgeType.NOT_ORIENTED),
-        new Edge(nodes[2], nodes[0], 100, EdgeType.DIRECTED)
+        new Edge(nodes[0], nodes[1], 100, v4()),
+        new Edge(nodes[0], nodes[1], 100, v4()),
+        new Edge(nodes[0], nodes[1], 100, v4()),
+        new Edge(nodes[1], nodes[2], 100, v4()),
+        new Edge(nodes[2], nodes[0], 100, v4())
       ]
       const graph = new Graph(nodes, edges)
 
