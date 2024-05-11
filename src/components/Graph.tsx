ea5a82159651f3fae24f6cf9fe1e6c6e7cb71b6c
icestormikk <jigalow.pavel2015@yandex.ru>
2024-05-11 19:58:12 +0300

    Обновил методы для визуализации графа

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,14 +1,13 @@
-import { useAppDispatch, useAppSelector } from '@/libs/redux/hooks';
-import { EdgeType } from '@/types/graph/EdgeType';
+import { useAppSelector } from '@/libs/redux/hooks';
 import React from 'react';
 import { ForceGraph3D } from 'react-force-graph';
 import SpriteText from 'three-spritetext';
 
 function Graph() {
-  const dispatch = useAppDispatch()
   const { edges, nodes } = useAppSelector((state) => state.graph)
   const [graphNodes, setGraphNodes] = React.useState<any[]>([])
   const [graphEdges, setGraphEdges] = React.useState<any[]>([])
+  const [edgesPath, setEdgesPath] = React.useState<any[]>([])
   const [graphWidth, setGraphWidth] = React.useState(window.innerWidth)
   const [graphHeight, setGraphHeight] = React.useState(window.innerHeight)
 
@@ -19,6 +18,14 @@ function Graph() {
         setGraphHeight(window.innerHeight)
       }
 
+      window.addEventListener('resize', onResize)
+      return () => window.removeEventListener('resize', onResize)
+    },
+    []
+  )
+
+  React.useEffect(
+    () => {
       const transformedEdges = []
       for (const node of nodes) {
         const neighbours = edges.filter((edge) => edge.source.id === node.id)
@@ -29,21 +36,10 @@ function Graph() {
             curvature: i * 0.3, 
             rotation: 0
           })
-          if (neighbours[i].type === EdgeType.NOT_ORIENTED) {
-            transformedEdges.push({
-              source: neighbours[i].destination.id, 
-              target: neighbours[i].source.id, 
-              curvature: i * -0.3, 
-              rotation: 0
-            })
-          }
         }
       }
       setGraphNodes(nodes.map((n) => ({id: n.id, label: n.label})))
       setGraphEdges(transformedEdges)
-
-      window.addEventListener('resize', onResize)
-      return () => window.removeEventListener('resize', onResize)
     },
     [nodes, edges]
   )
@@ -63,6 +59,7 @@ function Graph() {
           sprite.textHeight = 1;
           return sprite;
         }}
+        linkColor={(link) => link.color}
         nodeThreeObjectExtend={true}
         linkCurvature="curvature"
         linkCurveRotation="rotation"
