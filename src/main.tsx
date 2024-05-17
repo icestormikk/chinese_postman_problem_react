46061a3f58bbb9197ef3abb2f4db7c972bac6a14
icestormikk <jigalow.pavel2015@yandex.ru>
2024-05-17 13:41:27 +0300

    Очистка кода

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux'
-
-import './index.css'
-import './demos/ipc'
 import { store } from './libs/redux/store'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
