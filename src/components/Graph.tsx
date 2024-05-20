9d276043b2e7c48a649b907be9fd641a6758091a
icestormikk <jigalow.pavel2015@yandex.ru>
2024-05-20 17:54:53 +0300

    Обновил логику отображения рёбер

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -2,6 +2,7 @@ import { useAppSelector } from '@/libs/redux/hooks';
 import React from 'react';
 import { ForceGraph3D } from 'react-force-graph';
 import SpriteText from 'three-spritetext';
+import { Graph as GraphType } from '@/types/graph/Graph'
 import { EdgeType } from '@/types/graph/EdgeType';
 
 function Graph() {
@@ -27,22 +28,27 @@ function Graph() {
 
   React.useEffect(
     () => {
+      const graph = new GraphType(nodes, edges)
       const result = []
-      for (let i = 0; i < edges.length; i++) {
-        const edge = edges[i]
-        result.push({
-          source: edge.source.id,
-          target: edge.destination.id,
-          curvature: i * 0.3,
-          rotation: 0,
-        })
-        if (edge.type == EdgeType.NOT_ORIENTED) {
+
+      for (const node of graph.nodes) {
+        const neighbours = graph.edges.filter((edge) => edge.source.id == node.id)
+        for (let i = 0; i < neighbours.length; i++) {
+          const edge = neighbours[i]
           result.push({
-            source: edge.destination.id,
-            target: edge.source.id,
-            curvature: i * -0.3,
+            source: edge.source.id,
+            target: edge.destination.id,
+            curvature: i * 0.3,
             rotation: 0,
           })
+          if (edge.type == EdgeType.NOT_ORIENTED) {
+            result.push({
+              source: edge.destination.id,
+              target: edge.source.id,
+              curvature: i * -0.3,
+              rotation: 0,
+            })
+          }
         }
       }
 
