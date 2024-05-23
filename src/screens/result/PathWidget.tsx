8582c4bd3cccd08cb9bc5e24d21d37f92386c79b
icestormikk <jigalow.pavel2015@yandex.ru>
2024-05-23 22:16:30 +0300

    Добавил проверку на то, были ли посещены все рёбра

--- src/screens/result/PathWidget.tsx
+++ src/screens/result/PathWidget.tsx
@@ -1,13 +1,30 @@
 import { Edge } from "@/types/graph/Edge";
 import PathEdge from "./PathEdge";
+import { useAppSelector } from "@/libs/redux/hooks";
+import React from "react";
 
 interface PathWidgetProps {
   path: Array<Edge<number>>
 }
 
 const PathWidget = ({ path }: PathWidgetProps) => {
+  const { edges } = useAppSelector((state) => state.graph)
+
+  const isAllEdgesVisited = React.useCallback(
+    () => {
+      for (const edge of edges) {
+        if (path.find((e) => e.id === edge.id) == undefined) {
+          return false
+        }
+      }
+
+      return true
+    },
+    [edges, path]
+  )
+
   return (
-    <div className="space-y-2 my-6">
+    <div className="space-y-2 my-6 flex flex-col">
       <b>Оптимальный путь: </b>
       {
         path.map((edge, index) => (
@@ -15,6 +32,7 @@ const PathWidget = ({ path }: PathWidgetProps) => {
         ))
       }
       <b>{`Общая длина пути: ${path.map((edge) => edge.weight).reduce((a, b) => a + b, 0)}`}</b>
+      <b>{`Все рёбра посещены? - ${isAllEdgesVisited() ? 'Да' : 'Нет'}`}</b>
     </div>
   );
 };
